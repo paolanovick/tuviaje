95d021043e5d743aff29d63f5fc91033efbdd4d1
Paola Novick <paola.novick@davinci.edu.ar>
2025-04-21 20:55:49 -0300

    carrrusel

--- src/servicios/especificos/datosAgenciaSimulada.ts
+++ src/servicios/especificos/datosAgenciaSimulada.ts
@@ -34,7 +34,7 @@ export const datosAgenciaSimuladaBack: AgenciaBackData = {
   publicidad_color_secundario: "#CE93D8",
   publicidad_color_terciario: "#4A0072",
   publicidad_imagen_1: "paginaWeb/banner1.png",
-  publicidad_imagen_2: "paginaWeb/cruta.jpg",
+  publicidad_imagen_2: "paginaWeb/banner2.jpg",
   publicidad_imagen_3: "paginaWeb/ruta2.jpg",
 
   tarjetas_titulo: "Destinos recomendados",
